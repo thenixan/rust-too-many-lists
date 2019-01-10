0a82a80ff8c92bf0b077be7003c766e54152b2f2
Ilya Linnik <i.linnik@rambler-co.ru>
2019-01-10 13:32:44 +0300

    3.5

--- src/second.rs
+++ src/second.rs
@@ -11,6 +11,10 @@ type Link<T> = Option<Box<Node<T>>>;
 
 pub struct IntoIter<T>(List<T>);
 
+pub struct Iter<'a, T: 'a> {
+    next: Option<&'a Node<T>>,
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
@@ -47,6 +51,10 @@ impl<T> List<T> {
     pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
     }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_ref().map(|node| &**node) }
+    }
 }
 
 impl<T> Iterator for IntoIter<T> {
@@ -54,6 +62,17 @@ impl<T> Iterator for IntoIter<T> {
 
     fn next(&mut self) -> Option<<Self as Iterator>::Item> {
         self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.elem
+        })
     }
 }
 
@@ -117,5 +136,16 @@ mod test {
         assert_eq!(iter.next(), Some(3));
         assert_eq!(iter.next(), Some(2));
         assert_eq!(iter.next(), Some(1));
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 }
