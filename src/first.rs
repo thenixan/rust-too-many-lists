e8a0f183207368111fbb71efbcc888613538ed74
Ilya Linnik <i.linnik@rambler-co.ru>
2019-01-10 12:52:20 +0300

    2.4

--- src/first.rs
+++ src/first.rs
@@ -1,10 +1,12 @@
+use std::mem;
+
 pub struct List {
     head: Link,
 }
 
 struct Node {
     elem: i32,
-    next: List,
+    next: Link,
 }
 
 enum Link {
@@ -15,5 +17,12 @@ enum Link {
 impl List {
     fn new() -> List {
         List { head: Link::Empty }
+    }
+    fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node)
     }
 }
